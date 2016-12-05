8f28b36becb6ac50acd26e0f69f098b7b22bdfff
idob82 <ido.barav@gmail.com>
2016-12-05 11:13:37 +0200

    Add files via upload

---
+++ NadlanBekatanGame.py
@@ -0,0 +1,176 @@
+import random
+
+from numpy import sort
+
+from Phase1RandomBot import Phase1RandomBot
+from Phase1StupidBot import Phase1StupidBot
+
+from collections import defaultdict
+CARD_TAKEN = -1
+CARDS_TO_REMOVE=defaultdict(int)
+CARDS_TO_REMOVE[3] = 6
+STARTING_AMOUNT=defaultdict(int)
+STARTING_AMOUNT[3] = 24
+STARTING_AMOUNT[4] = 21
+
+
+class NadlanBekatanGame(object):
+    def run_game(self, bots, verbose=False):
+        number_of_bots = len(bots)
+        locations = range(1, 31)
+        cheques = [0, 0] + range(2, 16) + range(2, 16)
+        # Remove the needed amount random locations and cheques
+        for _ in range(CARDS_TO_REMOVE[number_of_bots]):
+            index = random.randint(0, len(locations) - 1)
+            cheques.pop(index)
+            locations.pop(index)
+
+        # Phase 1:
+        # Bidding wars
+        # Choose locations for bid
+        bots_money = [STARTING_AMOUNT[number_of_bots]] * number_of_bots
+        bots_cards = [[] for i in range(number_of_bots)]
+        [bot.init(STARTING_AMOUNT[number_of_bots]) for bot in bots]
+
+        starting_bot = 0
+        while len(locations) > 0:
+            starting_bot = self.bidding_round(starting_bot, bots_cards, bots_money, locations, bots, verbose)
+
+        if verbose:
+            print "Bots money after bidding stage:", bots_money
+            print "Cards", bots_cards
+
+        while len(cheques) > 0:
+            self.buying_round(bots_money, bots_cards, bots, cheques, verbose)
+
+        winner_amount = max(bots_money)
+        winners = [i for i in range(len(bots_money)) if bots_money[i] == winner_amount]
+
+        if verbose:
+            print "Amount of money left:", bots_money
+        return winners
+
+    def bidding_round(self, starting_bot, bots_cards, bots_money, locations, bots, verbose=False):
+        number_of_bots = len(bots)
+        locations_chosen = []
+        for i in range(number_of_bots):
+            index = random.randint(0, len(locations) - 1)
+            locations_chosen.append(locations[index])
+            locations.pop(index)
+        locations_chosen.sort()
+        if verbose:
+            print "START OF BIDDING ROUND. starting bot {}. Money: {}. locations: {}".format(starting_bot,
+                                                                                             bots_money, locations_chosen)
+        bots_in_play = range(number_of_bots)
+        bot_in_list_index = starting_bot
+        current_bid = 0
+        last_buyer = -1
+        bids = {}
+        for i in range(number_of_bots):
+            bids[i] = [0]
+
+        while len(locations_chosen) > 0:
+            bot_in_list_index %= len(bots_in_play)
+            current_bot_index = bots_in_play[bot_in_list_index]
+            current_bot = bots[current_bot_index]
+
+            bid_response = current_bot.bid(locations_chosen, bids, current_bid)
+
+            if bid_response.buy or not self.validate_bid_response(bid_response.value, bots_money[current_bot_index],
+                                                                  current_bid):
+                bot_bid = bids[current_bot_index][-1]
+                card_to_take = locations_chosen.pop(0)
+                if len(locations_chosen) == 0:
+                    price = bot_bid
+                else:
+                    price = bot_bid / 2
+                bots_money[current_bot_index] -= price
+                bots_cards[current_bot_index].append(card_to_take)
+
+                current_bot.buy(card_to_take, price)
+
+                # To allow full data - append None
+                bids[current_bot_index].append(CARD_TAKEN)
+                bots_in_play.pop(bot_in_list_index)
+                last_buyer = current_bot_index
+            else:
+                # bid_response is valid, and raises the bid
+                bids[current_bot_index].append(bid_response.value)
+                current_bid = bid_response.value
+                bot_in_list_index += 1
+        for bot in bots:
+            bot.end_bidding_round()
+        if verbose:
+            print "END OF ROUND. bids: {}".format(bids)
+        return last_buyer
+
+    @staticmethod
+    def buying_round(bots_money, bots_cards, bots, cheques, verbose=False):
+        number_of_bots = len(bots)
+        cheques_chosen = []
+        for _ in range(number_of_bots):
+            index = random.randint(0, len(cheques) - 1)
+            cheques_chosen.append(cheques[index])
+            cheques.pop(index)
+        sort(cheques_chosen)
+        if verbose:
+            print "START OF BUYING ROUND. cheques: {}. available cards: {}".format(cheques_chosen, bots_cards)
+
+        location_bid_mapping = {}
+
+        for index in range(len(bots)):
+            bot = bots[index]
+            location = bot.bid_for_cheques(cheques_chosen)
+            if location not in bots_cards[index]:
+                print "Invalid location chosen. Choosing first not in use"
+                location = bots_cards[index][0]
+            bots_cards[index].remove(location)
+            location_bid_mapping[location] = index
+
+        sorted_locations = sorted(location_bid_mapping.keys())
+        for index in range(len(sorted_locations)):
+            location = sorted_locations[index]
+            bot_index = location_bid_mapping[location]
+            bots_money[bot_index] += cheques_chosen[index]
+            bots[bot_index].buy_cheque(location, cheques_chosen[index], location_bid_mapping)
+
+        for bot in bots:
+            bot.end_buying_round()
+        if verbose:
+            print "END OF ROUND. Locations used: {}".format(sorted_locations)
+
+    @staticmethod
+    def validate_bid_response(value, bot_money, current_bid):
+        return current_bid < value <= bot_money
+
+
+game_server = NadlanBekatanGame()
+
+def tournament(bots, number_of_rounds=100):
+    tournament_results = [0] * len(bots)
+    # Do 100 rounds
+    for i in range(number_of_rounds):
+        winners = game_server.run_game(bots)
+        for w in winners:
+            tournament_results[w] += 1.0/len(winners)
+    return tournament_results
+
+
+bots = [Phase1StupidBot("Danny"), Phase1StupidBot("OtherDanny"), Phase1RandomBot("NotDanny", 0.4)]
+winner = game_server.run_game(bots, verbose=True)
+
+print "Winners for first game are:", winner
+
+print "Starting tournament", bots
+tournament_results = tournament(bots)
+for i in range(len(bots)):
+    print bots[i], "won", tournament_results[i], "rounds"
+
+bots = [Phase1StupidBot("Danny"), Phase1RandomBot("OtherNotDanny", 0.4), Phase1RandomBot("NotDanny", 0.4)]
+
+print "Starting tournament", bots
+tournament_results = tournament(bots)
+for i in range(len(bots)):
+    print bots[i], "won", tournament_results[i], "rounds"
+
+
---
+++ Phase1StupidBot.py
@@ -0,0 +1,41 @@
+from Utils import BidResponse
+
+
+class Phase1StupidBot(object):
+
+    def __init__(self, name):
+        self.name = name
+        self.money = 0
+        self.cards = []
+
+    def __str__(self):
+        return "Stupid bot {}".format(self.name)
+
+    def __repr__(self):
+        return str(self)
+
+    def init(self, money):
+        self.money = money
+        self.cards = []
+
+    def bid(self, locations, previous_bids, current_bid):
+        if len(locations) == 1 or current_bid >= self.money:
+            return BidResponse(True)
+        return BidResponse(bid=current_bid + 1)
+
+    def buy(self, card, price):
+        self.money -= price
+        self.cards.append(card)
+
+    def end_bidding_round(self):
+        self.cards.sort()
+
+    def bid_for_cheques(self, cheques_chosen):
+        return self.cards[0]
+
+    def buy_cheque(self, location, cheque, sorted_locations):
+        self.money += cheque
+        self.cards.remove(location)
+
+    def end_buying_round(self):
+        pass
