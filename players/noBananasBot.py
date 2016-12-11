0710daae68af7b4992e575b34a170d1a50b40c32
JWSTelescope <ido.winter@gmail.com>
2016-12-11 17:16:09 +0200

    Created the new bot.

--- players/noBananasBot.py
+++ players/noBananasBot.py
@@ -1,13 +1,14 @@
-import random
-
 from playerbase import PlayerBase
-from players_prototypes.RandomBotPrototype import RandomBotPrototype
 from utils import BidResponse
 
-
-class noBananasBot(RandomBotPrototype):
-    def __init__(self):
-        super(noBananasBot, self).__init__(0.6)
+class NoBananasBot(PlayerBase):
 
     def __str__(self):
         return "No Bananas Bot"
+
+    def bid(self, player_state, available_locations, current_bid_by_player_id, current_highest_bid):
+        pass
+
+    def bid_for_cheques(self, player_state, available_cheques):
+        pass
+
