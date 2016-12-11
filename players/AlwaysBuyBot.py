8dcb0b291b6ddff393bb0f033cc20cd5807288d7
idbarav <idbarav@microsoft.com>
2016-12-11 17:59:44 +0200

    Default - fix always buy bot

--- players/AlwaysBuyBot.py
+++ players/AlwaysBuyBot.py
@@ -7,9 +7,7 @@ class AlwaysBuyBot(PlayerBase):
         return "Always Buy Bot"
 
     def bid(self, player_state, available_locations, current_bid_by_player_id, current_highest_bid):
-        if current_highest_bid >= player_state.money:
-            return BidResponse(forfeit=True)
-        return BidResponse(bid=current_highest_bid + 1)
+        return BidResponse(forfeit=True)
 
     def bid_for_cheques(self, player_state, available_cheques):
         return min(player_state.cards)
